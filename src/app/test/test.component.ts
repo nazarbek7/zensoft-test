55a313c85e3e760cfa313e9349ed9f0569b1ac14
Nazarbek Erkinov <nazarbek.erkinov@gmail.com>
2019-05-31 05:36:37 +0600

    add test

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,6 +9,65 @@ export class TestComponent implements OnInit {
 
   constructor() { }
 
+  file1: any;
+  file2: any;
+  matchMode1: string;
+  matchMode2: string;
+
+  uploadFirstFile(fileList: FileList) {
+    const file = fileList[0];
+    const fileReader: FileReader = new FileReader();
+    const _this = this;
+    fileReader.onloadend = function(x) {
+      _this.file1 = fileReader.result;
+    };
+    fileReader.readAsText(file);
+  }
+
+  uploadSecondFile(fileList: FileList) {
+    const file = fileList[0];
+    const fileReader: FileReader = new FileReader();
+    const _this = this;
+    fileReader.onloadend = function(x) {
+      _this.file2 = fileReader.result;
+    };
+    fileReader.readAsText(file);
+  }
+
+  testFilesMatching() {
+    const array1 = this.file1.split('\n');
+    const array2 = this.file2.split('\n');
+
+    let isMatch = '';
+
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array1[i] === array2[j]) {
+          isMatch = isMatch + array1[i] + '\n';
+        }
+      }
+    }
+
+    this.matchMode1 = isMatch;
+
+    isMatch = '';
+
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        const child1 = array1[i];
+        const child2 = array2[j];
+
+        if ((child1.length > 0 && child2.length > 0)) {
+          if (child1.includes(child2) || child2.includes(child1)) {
+            isMatch = isMatch + array1[i] + '\n';
+          }
+        }
+      }
+    }
+
+    this.matchMode2 = isMatch;
+  }
+
   ngOnInit() {
   }
 
